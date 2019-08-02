fe091a866c9c86d61e04c3a289135fb53eb57b67
Vvvvvvvvvvvvvvvvicky <vicky.h.m.lee@outlook.com>
2019-08-02 23:58:22 +0800

    update

---
+++ src/test/resources/sql/customer_init.sql
@@ -0,0 +1,7 @@
+truncat customer;
+
+INSERT INTO customer(`name`, `contact`, `telephone`, `email`, `remark`)
+VALUES ('custmer1', 'Abby', '13632371520', 'jack@163.com', NULL);
+
+INSERT INTO customer(`name`, `contact`, `telephone`, `email`, `remark`)
+VALUES ('custmer1', 'Abby', '13632371520', 'jack@163.com', NULL);
